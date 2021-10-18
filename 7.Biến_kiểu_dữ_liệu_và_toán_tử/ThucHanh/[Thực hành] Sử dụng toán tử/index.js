cbcdd5b342c5b4b9f71a5ca328c4fc775d0d058b
Dang Nghia <you@example.com>
2021-10-18 18:57:29 +0700

    19g 18/10

---
+++ 7.Biến_kiểu_dữ_liệu_và_toán_tử/ThucHanh/[Thực hành] Sử dụng toán tử/index.js
@@ -0,0 +1,9 @@
+
+let inputWidth = parseInt(prompt("Enter the width"));
+document.writeln('Width =  ' + inputWidth );
+document.write('<br/>')
+let inputHeight = parseInt(prompt("Enter the height"));
+document.writeln('Height = ' + inputHeight );
+document.write('<br/>')
+let area = inputWidth*inputHeight;
+document.writeln("The area is  " + area);
